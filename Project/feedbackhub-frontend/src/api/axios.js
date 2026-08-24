09fbfeb9cf078589a1f7379344fbab347e9c2533
meetgondaliya31 <itsleo31x@gmail.com>
2026-08-24 09:52:30 +0530

    fd

---
+++ Project/feedbackhub-frontend/src/api/axios.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+// Create configured Axios client instance
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+// Request Interceptor: Automatically attach JWT Bearer token from localStorage
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('feedbackhub_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Response Interceptor: Handle global 401 Unauthorized errors
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Token is invalid or expired
+      localStorage.removeItem('feedbackhub_token');
+      localStorage.removeItem('feedbackhub_user');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default API;
